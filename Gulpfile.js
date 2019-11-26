d4af99bf999aeaac62f4c52f3676c56baa32c6a7
Ceda <pleskac@gmail.com>
2019-11-26 10:32:51 +0100

    Fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,14 @@
 var gulp        = require('gulp');
 var babel       = require('gulp-babel');
 var eslint       = require('gulp-eslint');
+var mocha       = require('gulp-mocha');
 var del         = require('del');
 
-function clean () {
-    return del(['lib', '.screenshots']);
-}
+gulp.task('clean', () => {
+    return del(['lib']);
+});
 
-function lint () {
+gulp.task('lint', () => {
     return gulp
         .src([
             'src/**/*.js',
@@ -17,14 +18,23 @@ function lint () {
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-}
+});
 
-function build () {
+gulp.task('build', () => {
     return gulp
         .src('src/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest('lib'));
-}
+});
 
-exports.lint  = lint;
-exports.build = gulp.parallel(lint, gulp.series(clean, build));
+
+gulp.task('test', gulp.series('build', () => {
+    return gulp
+        .src('test/**.js')
+        .pipe(mocha({
+            ui:       'bdd',
+            reporter: 'spec',
+            timeout:  typeof v8debug === 'undefined' ? 2000 : Infinity // NOTE: disable timeouts in debug
+        }));
+
+}));
